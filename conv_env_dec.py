34b909f827829ff6163863e5c37363ea8ecd04b5
diegoPasini <dpasini@wharton.upenn.edu>
2024-08-30 16:28:53 -0400

    Comments

--- conv_env_dec.py
+++ conv_env_dec.py
@@ -6,6 +6,8 @@ class ConvEncoder(nn.Module):
     def __init__(self, input_channels, feature_dim):
         super(ConvEncoder, self).__init__()
         self.relu = nn.ReLU()
+        
+        ## We choose an enc of input channels -> 32 -> 64 -> 128 -> 256
         self.conv32 = nn.Conv2d(input_channels, 32, kernel_size=4, stride=2, padding=1)
         self.conv64 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1)
         self.conv128 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1)
@@ -17,6 +19,7 @@ class ConvEncoder(nn.Module):
         x = self.relu(self.conv64(x))
         x = self.relu(self.conv128(x))
         x = self.relu(self.conv256(x))
+        ## Change dim to feature vector
         x = x.view(x.shape[0], -1)
         x = self.fc(x)
         return x
@@ -25,7 +28,11 @@ class ConvDecoder(nn.Module):
     def __init__(self, feature_dim, output_channels):
         super(ConvDecoder, self).__init__()
         self.relu = nn.ReLU()
+
+        ## ! Is sigmoid the correct activation function for this? 
         self.sigmoid = nn.Sigmoid()
+
+        ## Dec 256 -> 128 -> 64 -> 32 -> output_channels
         self.fc = nn.Linear(feature_dim, 256 * 4 * 4)
         self.deconv256 = nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1)
         self.deconv128 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1)
