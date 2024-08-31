baa8705d3c9198db6e85e7a9e60b6e1a3dd953bd
diegoPasini <dpasini@wharton.upenn.edu>
2024-08-31 15:26:21 -0400

    more bug fixes

--- Dreamer.py
+++ Dreamer.py
@@ -214,7 +214,7 @@ class Dreamer(nn.Module):
             done = False
             action = self.sample_action(torch.cat([self.prev_state, self.prev_latent_space]))
             timestep = self.env.step(action)
-            obs = torch.tensor(self.env.physics.render(camera_id=0, height=120, width=160))
+            obs = torch.tensor(self.env.physics.render(camera_id=0, height=120, width=160).copy())
             if (t == self.batch_train_freq):
                 done = True
             latent_spaces, prior_states, prior_means, prior_std_devs, \
@@ -242,7 +242,8 @@ class Dreamer(nn.Module):
         self.num_points = num_points
         self.data_length = data_length
         obs = self.env.reset()
-        self.last_obs = torch.tensor(self.env.physics.render(camera_id=0, height=120, width=160)).to(device)
+        render = self.env.physics.render(camera_id=0, height=120, width=160)
+        self.last_obs = torch.tensor(render.copy()).to(device)
         self.prev_state = torch.zeros((self.batch_size, self.RSSM.state_dim))
         self.prev_latent_space = torch.zeros((self.batch_size, self.RSSM.latent_dim))
 
